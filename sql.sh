79afc0f13f4765903f7a892c288b15446860bd45
Timotheus Schöne <w45ej7@arcor.de>
2016-03-18 22:41:31 +0100

    Update sql.(sh,pref)

--- sql.sh
+++ sql.sh
@@ -4,7 +4,9 @@ source sql.pref
 
 #Ausgabe der Werte aus sql.pref
 echo "DB_HOST=$DB_HOST"
-echo "DB_DATABASE=$DB_DATABASE"
+echo "DB_NAME=$DB_NAME"
 echo "DB_USER=$DB_USER"
 echo "DB_PWD=$DB_PWD"
 echo "BACKUP_DIR=$BACKUP_DIR"
+
+mysqldump --lock-tables -h $DB_HOST -u $DB_USER -p$DB_PWD $DB_NAME > $BACKUP_DIR/backup$(date +%Y-%m-%d-%H.%M.%S).sql
